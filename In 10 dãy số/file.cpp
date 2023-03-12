e59e7a835bec69786b0ac2d7f1ab3ea3616a5051
Anh Havertz <121730782+Anh-Havertz@users.noreply.github.com>
2023-03-12 12:01:40 +0700

    Update file.cpp

--- In 10 dãy số/file.cpp
+++ In 10 dãy số/file.cpp
@@ -4,25 +4,26 @@
 #include <ctime>
 #include <vector>
 #include <iostream>
+using namespace std; 
 
 int main() {
     constexpr int kNumArrays = 10;
     constexpr int kArraySize = 1000000;
-    std::srand(std::time(nullptr));
-    std::array<std::vector<double>, kNumArrays> data;
+    srand(time(nullptr));
+    array<vector<double>, kNumArrays> data;
     for (auto& arr : data) {
         arr.resize(kArraySize);
         for (auto& elem : arr) {
-            elem = static_cast<double>(std::rand()) / RAND_MAX;
+            elem = static_cast<double>(rand()) / RAND_MAX;
         }
     }
-    std::sort(data[0].begin(), data[0].end());
-    std::sort(data[1].begin(), data[1].end(), std::greater<double>());
+    sort(data[0].begin(), data[0].end());
+    sort(data[1].begin(), data[1].end(), greater<double>());
      for (const auto& arr : data) {
         for (const auto& elem : arr) {
-            std::cout << elem << ' ';
+           cout << elem << ' ';
         }
-        std::cout << '\n';
+         cout << '\n';
     }
 }
 
